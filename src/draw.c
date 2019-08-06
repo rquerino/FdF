175d028ae5f1c9295756f3f10f1aae48e835d655
Renato Querino <renatoqrn@gmail.com>
2019-08-06 16:54:47 -0700

    fdf evolution, kinda working

---
+++ src/draw.c
@@ -0,0 +1,290 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rquerino <rquerino@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/27 22:42:23 by rquerino          #+#    #+#             */
+/*   Updated: 2019/08/06 16:29:26 by rquerino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fdf.h"
+
+/*
+** This will be the start of my own perspective of bresenham algorithm
+** 
+*/
+/*
+void    ft_bresenham(t_fdf *fdf)
+{
+    int dy;
+    int dx;
+    int f;
+
+    dy = fdf->map.y1 - fdf->map.y0;
+    dx = fdf->map.x1 - fdf->map.x0;
+    f = (dx > dy ? dx : dy) / 2;
+    mlx_pixel_put(fdf->mlx.init, fdf->mlx.win, fdf->map.x0, fdf->map.y0, fdf->map.color);
+    while (fdf->map.x0 < fdf->map.x1 || fdf->map.y0 < fdf->map.y1)
+    {
+        if (f < 0)
+            f = f + (dx > dy ? dx : dy) - (dx < dy ? dx : dy);
+        else
+            f -= (dx < dy ? dx : dy);
+        if (f > 0)
+        {
+            if (dx > dy)
+                fdf->map.x0 += 1;
+            else
+                fdf->map.y0 += 1;
+        }
+        else
+        {
+            fdf->map.x0 += 1;
+            fdf->map.y0 += 1; 
+        }
+        mlx_pixel_put(fdf->mlx.init, fdf->mlx.win, fdf->map.x0, fdf->map.y0, fdf->map.color);
+    }
+}*/
+
+
+void    ft_bresenham(t_fdf *fdf)
+{
+	double x;
+	double y;
+	double dx;
+	double dy;
+	double unitvector;
+
+	x = fdf->map.x0;
+	y = fdf->map.y0;
+	dx = fdf->map.x1 - fdf->map.x0;
+	dy = fdf->map.y1 - fdf->map.y0;
+	unitvector = sqrt((pow(dx, 2)) + (pow(dy, 2)));
+	dx /= unitvector;
+	dy /= unitvector;
+	while (unitvector > 0)
+	{
+		//put_pixel(fdf, y, z, uvector);
+        mlx_pixel_put(fdf->mlx.init, fdf->mlx.win, x, y, fdf->map.color);
+		x += dx;
+		y += dy;
+		unitvector -= 1;
+	}
+}
+
+/*
+** Function to draw lines in y axis.
+** The formula for the x axis is:
+**
+** The formula for the y axis is:
+**
+*/
+
+void    ft_drawvertical(t_fdf *fdf, int y, int x)
+{
+    int xt;
+    int yt;
+
+    xt = x - fdf->map.width / 2;
+    yt = y - fdf->map.height / 2;
+    fdf->map.x0 = (fdf->map.angle_x * (xt - yt) * fdf->map.scale) +
+        (WIDTH / 2) + fdf->map.coord_x;
+    fdf->map.x1 = (fdf->map.angle_x * (xt - (yt + 1)) * fdf->map.scale) +
+        (WIDTH / 2) + fdf->map.coord_x;
+    fdf->map.y0 = (fdf->map.angle_y * (xt + yt) * fdf->map.scale) -
+        (fdf->map.values[y][x] * fdf->map.z_multiplier) +
+        (HEIGHT / 2) + fdf->map.coord_y;
+    fdf->map.y1 = (fdf->map.angle_y * (xt + (yt + 1)) * fdf->map.scale) -
+        (fdf->map.values[y + 1][x] * fdf->map.z_multiplier) +
+        (HEIGHT / 2) + fdf->map.coord_y;
+    ft_bresenham(fdf);
+}
+
+/*
+** Function to draw lines in x axis.
+** The formula for the x axis is:
+**
+** The formula for the y axis is:
+**
+*/
+
+void    ft_drawhorizontal(t_fdf *fdf, int y, int x)
+{
+    int xt;
+    int yt;
+
+    xt = x - fdf->map.width / 2;
+    yt = y - fdf->map.height / 2;
+    fdf->map.x0 = (fdf->map.angle_x * (xt - yt) * fdf->map.scale) +
+        (WIDTH / 2) + fdf->map.coord_x;
+    fdf->map.x1 = (fdf->map.angle_x * ((xt + 1) - yt) * fdf->map.scale) +
+        (WIDTH / 2) + fdf->map.coord_x;
+    fdf->map.y0 = (fdf->map.angle_y * (xt + yt) * fdf->map.scale) -
+        (fdf->map.values[y][x] * fdf->map.z_multiplier) +
+        (HEIGHT / 2) + fdf->map.coord_y;
+    fdf->map.y1 = (fdf->map.angle_y * ((xt + 1) + yt) * fdf->map.scale) -
+        (fdf->map.values[y][x + 1] * fdf->map.z_multiplier) +
+        (HEIGHT / 2) + fdf->map.coord_y;
+    ft_bresenham(fdf);
+}
+
+int     ft_drawmap(t_fdf *fdf)
+{
+    int y;
+    int x;
+
+    y = 0;
+    mlx_clear_window(fdf->mlx.init, fdf->mlx.win);
+    while (y < fdf->map.height)
+    {
+        x = 0;
+        while (x < fdf->map.width)
+        {
+            fdf->map.x0 = x;
+            fdf->map.y0 = y;
+            if (fdf->map.width > x + 1)
+                ft_drawhorizontal(fdf, y, x);
+            if (fdf->map.height > y + 1)
+                ft_drawvertical(fdf, y, x);
+            x++;
+        }
+        y++;
+    }
+    return (0);
+}
+
+
+/*
+
+void    ft_bresenham(t_fdf *fdf)
+{
+    int dx;
+    int dy;
+    int p;
+    //int x;
+    //int y;
+ 
+	dx = fdf->map.x1 - fdf->map.x0;
+	dy = fdf->map.y1 - fdf->map.y0;
+	//x = fdf->map.x0;
+	//y = fdf->map.y0;
+	p = 2 * dy - dx;
+	while(fdf->map.x0 < fdf->map.x1)
+	{
+		if(p >= 0)
+		{
+            mlx_pixel_put(fdf->mlx.init, fdf->mlx.win, fdf->map.x0, fdf->map.y0, fdf->map.color);
+			fdf->map.y0 += 1;
+			p = p + 2 * dy - 2 * dx;
+		}
+		else
+		{
+			mlx_pixel_put(fdf->mlx.init, fdf->mlx.win, fdf->map.x0, fdf->map.y0, fdf->map.color);
+			p = p + 2 * dy;
+		}
+		fdf->map.x0 += 1;
+	}
+}
+*/
+/*
+** Multiplies Z axis(values[y][x]) * z_multiplier (increase/decrease depth)
+** Also multiplies by the scale
+** returns the y coordinate (still without angles)
+*/
+/*
+int     ft_coordy(t_fdf *fdf, int y, int x, int z)
+{
+    z *= fdf->map.z_multiplier;
+    if (z > fdf->map.zmax)
+        z = fdf->map.zmax;
+    if (z < fdf->map.zmin)
+        z = fdf->map.zmin;
+    //return (fdf->map.coord_y + ((fdf->map.scale * x) + (fdf->map.scale * y) - (z * 2)));
+
+    return ((fdf->map.xangle * ((x - fdf->map.width / 2) + (y - fdf->map.height / 2))
+        * fdf->map.scale) - (fdf->map.values[y][x] * fdf->map.z_multiplier)
+        + (HEIGHT / 2) + fdf->map.coord_y);
+}
+
+int     ft_coordx(t_fdf *fdf, int y, int x)
+{
+    //return (fdf->map.coord_x - (fdf->map.scale * y) + (fdf->map.scale * x));
+    return ((fdf->map.xangle * ((x - fdf->map.width / 2) - (y - fdf->map.height / 2))
+        * fdf->map.scale) + ((WIDTH / 2) + fdf->map.coord_x));
+}
+*/
+/*
+int     ft_coordy(t_fdf *fdf, int y, int x, int z)
+{
+    z *= fdf->map.z_multiplier;
+    if (z > fdf->map.zmax)
+        z = fdf->map.zmax;
+    if (z < fdf->map.zmin)
+        z = fdf->map.zmin;
+    return ((-z * fdf->map.scale + (x * fdf->map.scale + y * fdf->map.scale) 
+        * fdf->map.yangle) + fdf->map.coord_y);
+}
+
+int     ft_coordx(t_fdf *fdf, int y, int x)
+{
+    return (((x * fdf->map.scale - y * fdf->map.scale) * fdf->map.xangle) + fdf->map.coord_x);
+}
+*/
+/*
+void    ft_drawvertical(t_fdf *fdf)
+{
+    int y;
+    int x;
+
+    y = 0;
+    while (y < fdf->map.height)
+    {
+        x = 0;
+        while (x < fdf->map.width - 1)
+        {
+            fdf->map.x0 = ft_coordx(fdf, y, x);
+            fdf->map.y0 = ft_coordy(fdf, y, x, fdf->map.values[y][x]);
+            fdf->map.x1 = ft_coordx(fdf, y, x + 1);
+            fdf->map.y1 = ft_coordy(fdf, y, x + 1, fdf->map.values[y][x + 1]);
+            ft_bresenham(fdf);
+            x++;
+        }
+        y++;
+    }    
+}
+
+void    ft_drawhorizontal(t_fdf *fdf)
+{
+    int y;
+    int x;
+
+    y = 0;
+    while (y < fdf->map.height - 1)
+    {
+        x = 0;
+        while (x < fdf->map.width)
+        {
+            fdf->map.x0 = ft_coordx(fdf, y, x);
+            fdf->map.y0 = ft_coordy(fdf, y, x, fdf->map.values[y][x]);
+            fdf->map.x1 = ft_coordx(fdf, y + 1, x);
+            fdf->map.y1 = ft_coordy(fdf, y + 1, x, fdf->map.values[y + 1][x]);
+            ft_bresenham(fdf);
+            x++;
+        }
+        y++;
+    }
+}
+*/
+/*
+** Function to draw the map.
+int    ft_drawmap(t_fdf *fdf)
+{
+    mlx_clear_window(fdf->mlx.init, fdf->mlx.win);
+    ft_drawhorizontal(fdf);
+    ft_drawvertical(fdf);
+    return (0);
+}
+*/
--- src/fdf.c
+++ src/fdf.c
@@ -6,15 +6,15 @@
 /*   By: rquerino <rquerino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 09:22:13 by rquerino          #+#    #+#             */
-/*   Updated: 2019/07/19 09:54:02 by rquerino         ###   ########.fr       */
+/*   Updated: 2019/08/06 16:44:27 by rquerino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-/*
-** To compile : cc -I minilibx_macos/ src/fdf.c src/map.c src/fdf.h libft/libft.a 
-** -L minilibx_macos/ -lmlx -framework OpenGL -framework AppKit
+/* 
+cc -I minilibx_macos/ src/fdf.c src/map.c src/draw.c includes/fdf.h libft/libft.a 
+-L minilibx_macos/ -lmlx -framework OpenGL -framework AppKit
 */
 
 /*
@@ -23,38 +23,60 @@
 ** For bonus, ANY other functions if justified.
 */
 
-void    ft_keys()
+static void	ft_color(t_fdf *fdf)
 {
-    ft_putstr("key pressed\n");
+	fdf->map.color = (rand() % 0xFFFFFF);
 }
 
-void    ft_originalmap(t_fdf *fdf)
+void    ft_createmap(t_fdf *fdf)
 {
-    fdf->color.R = 0;
-    fdf->color.G = 255;
-    fdf->color.B = 0;
+    fdf->map.coord_x = 0; //650;
+    fdf->map.coord_y = 0; //40;
+    fdf->map.z_multiplier = 1.00; //For the increase/decrease on Z axis(values)
+	fdf->map.angle_x = cos(0.523599); //cos(PI / 3); //0.5
+	fdf->map.angle_y = sin(0.523599); //fdf->map.angle_x * sin(PI / 6); //0.5 * 0.5 = 0.25
+	if (ceil((fdf->map.width > fdf->map.height)))
+		fdf->map.scale = WIDTH / fdf->map.width;
+	else
+		fdf->map.scale = HEIGHT / fdf->map.height;	
+	fdf->map.camera = 1;
+    fdf->map.color = 0xFFFFFF;
 }
 
-
-void    ft_printvalues(t_fdf *fdf)
+static int	ft_funcs(int key, t_fdf *fdf)
 {
-    int i;
-    int j;
-
-    i = 0;
-    j = 0;
-    while (i < 11)
-    {
-        while (j < 18)
-        {
-            printf("%02d ,", fdf->map.values[i][j]);
-            j++;
-        }
-        j = 0;
-        i++;
-        printf("\n");
-    }
+	if (key == KEY_ESC)
+		exit(0);
+	else if (key == KEY_ANSI_R)
+		ft_createmap(fdf);
+	else if (key == KEY_ANSI_M)
+		ft_color(fdf);
+	else if (key == KEY_ANSI_W)
+		fdf->map.coord_y -= 5;
+	else if (key == KEY_ANSI_A)
+		fdf->map.coord_x += 5;
+	else if (key == KEY_ANSI_S)
+		fdf->map.coord_y += 5;
+	else if (key == KEY_ANSI_D)
+		fdf->map.coord_x -= 5;
+	else if (key == KEY_ANSI_EQUAL)
+		fdf->map.scale += 5;
+	else if ((key == KEY_ANSI_MINUS) && (fdf->map.scale > 5))
+		fdf->map.scale -= 5;
+	else if ((key == KEY_UPARROW) && (fdf->map.z_multiplier < 10))
+		fdf->map.z_multiplier += 0.5;
+	else if ((key == KEY_DOWNARROW) && (fdf->map.z_multiplier > -10))
+		fdf->map.z_multiplier -= 0.5;
+	else if (key == KEY_ANSI_C)
+		fdf->map.angle_y *= (fdf->map.camera++ % 2) ? 0.2 : 5;
+	return (0);
 }
+
+/*
+** Handle: without parameters, too many parameters, file that doesn't exist.
+** Tests: 0 x 0 map, 4x4 map, 1 value to see the difference in z axis
+** Test with 42 map, random 16 x 16 map, bigger and heavier maps
+*/
 
 int     main(int ac, char **av)
 {
@@ -64,15 +86,92 @@ int     main(int ac, char **av)
     {
         fdf = malloc(sizeof(t_fdf));
         ft_readmap(fdf, av[1]);
-        ft_printvalues(fdf);
-        printf("width: %d, height: %d\n", fdf->map.width, fdf->map.height);
+        ft_createmap(fdf);
         fdf->mlx.init = mlx_init();
         fdf->mlx.win = mlx_new_window(fdf->mlx.init, WIDTH, HEIGHT, "FdF");
+		mlx_hook(fdf->mlx.win, 2, 3, ft_keys, fdf);
+        mlx_loop_hook(fdf->mlx.init, ft_drawmap, fdf);
         mlx_loop(fdf->mlx.init);
-        mlx_hook(fdf->mlx.win, 2, 3, ft_keys, fdf);
         ft_freeall(fdf);
     }
     else
-        ft_putstr("Please use a valid file.\n");
+        ft_putstr("Please use a single valid file.\n");
     return (0);
 }
+
+
+/* FDF CORRECTION
+
+sections
+preliminaries
+Preliminary tests
+REMEMBER THAT YOU MUST CORRECATE ONLY WHAT IS ON THE DEPOSIT OF
+RENDERED BY THE STUDENT.
+This is to make a "git clone" of the rendering depot, and correct what is there.
+Check:
+- No rendering (= nothing in the deposit git)
+- No author file
+- Invalid author file
+- Standard (with the norminette)
+- Cheats (and in particular the prohibited functions, unless justified in the context of a
+bonus)
+- Presence after compilation of the binary fdf
+And of course, the evaluation stops if segfault or other uncontrolled error while it
+could be. Attention, errors of the X server can not be diverted.
+
+Some are due to user actions that can not be controlled (closing of
+the window), others to bad programming, and that's not good. And the minilibX
+native does not use the X server.
+features
+
+Error management
+Test the fdf without parameter, with too many parameters, a file that does not exist or on
+which you do not have rights. If the management is correct, then it's good.
+
+Graphic
+- A window appears
+- Something is trace in the window
+- ESC is pressed and the program is taken out of the program
+- When we receive the event Expose, we retrace what is in the window
+
+Trace
+- Use a flat map with only 0, 4x4: we have a flat wire grid with a
+projection that is used to give an idea of 3D
+- Same map, with 1 point at a different altitude. Check that the rendering matches, and
+that the impression of relief is present
+
+Heavy
+- Check the rendering of maps a little bigger: 42.fdf provided with the subject, and a map
+16x16 random
+
+From the heaviest
+- Test with bigger, heavier maps. Either among those provided with the subject,
+be yours. Be logical and stay compatible between what might contain your
+map (more colors for example) and what the program can do to correct
+
+Some are due to user actions that can not be controlled (closing of
+the window), others to bad programming, and that's not good. And the minilibX
+native does not use the X server.
+features
+Error management
+Test the fdf without parameter, with too many parameters, a file that does not exist or on
+which you do not have rights. If the management is correct, then it's good.
+
+Graphic responsive
+- If during the test preceded with heavy maps, the graph remains fluid and pleasant
+it's cool
+
+bonus
+According to the bonuses present, give more or less points:
+- The size of the window is determined according to the size of the map
+- The size of the grid tiles changes to fit the size of the window
+(- Both are possible at the same time)
+- Colors, possibly according to the altitude
+- Hidden Faces
+- Zoom in / out (with the mouse wheel for example)
+- Rotation by 90 degrees
+- Fluid rotation of any angle
+- Any position of the eye
+- Several possible projections
+- lots of other stuff
+ */
